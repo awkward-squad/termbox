9f6a5fc95d6c4347b80cc10679d1a39ef8be8721
Mitchell Rosen <mitchellwrosen@gmail.com>
2023-11-05 23:55:27 -0500

    fill out more functions

--- termbox2-bindings-c/src/Termbox2/Bindings/C/Internal/Cell.hs
+++ termbox2-bindings-c/src/Termbox2/Bindings/C/Internal/Cell.hs
@@ -6,12 +6,10 @@ where
 import Data.Word (Word16, Word32)
 import Foreign.C (CSize)
 import Foreign.Ptr (Ptr)
-import Foreign.Storable (Storable (..))
-import qualified Foreign.Storable as Storable
 import GHC.Generics (Generic)
 
 -- | A cell.
-data Tb_cell = Tb_cell
+data {-# CTYPE "termbox2.h" "struct tb_cell" #-} Tb_cell = Tb_cell
   { -- | A unicode character.
     ch :: {-# UNPACK #-} !Word32,
     -- | Foreground attribute.
@@ -26,39 +24,3 @@ data Tb_cell = Tb_cell
     cech :: {-# UNPACK #-} !CSize
   }
   deriving stock (Eq, Generic, Ord, Show)
-
-instance Storable Tb_cell where
-  sizeOf :: Tb_cell -> Int
-  sizeOf _ =
-    8 + sizeofPtr + sizeofCSize + sizeofCSize
-
-  alignment :: Tb_cell -> Int
-  alignment _ =
-    4
-
-  peek :: Ptr Tb_cell -> IO Tb_cell
-  peek ptr =
-    Tb_cell
-      <$> Storable.peekByteOff ptr 0
-      <*> Storable.peekByteOff ptr 4
-      <*> Storable.peekByteOff ptr 6
-      <*> Storable.peekByteOff ptr 8
-      <*> Storable.peekByteOff ptr (8 + sizeofPtr)
-      <*> Storable.peekByteOff ptr (8 + sizeofPtr + sizeofCSize)
-
-  poke :: Ptr Tb_cell -> Tb_cell -> IO ()
-  poke ptr Tb_cell {ch, fg, bg, ech, nech, cech} = do
-    Storable.pokeByteOff ptr 0 ch
-    Storable.pokeByteOff ptr 4 fg
-    Storable.pokeByteOff ptr 6 bg
-    Storable.pokeByteOff ptr 8 ech
-    Storable.pokeByteOff ptr (8 + sizeofPtr) nech
-    Storable.pokeByteOff ptr (8 + sizeofPtr + sizeofCSize) cech
-
-sizeofPtr :: Int
-sizeofPtr =
-  sizeOf (undefined :: Ptr Word32)
-
-sizeofCSize :: Int
-sizeofCSize =
-  sizeOf (undefined :: CSize)
--- termbox2-bindings-c/src/Termbox2/Bindings/C/Internal/Functions.hs
+++ termbox2-bindings-c/src/Termbox2/Bindings/C/Internal/Functions.hs
@@ -19,10 +19,7 @@ module Termbox2.Bindings.C.Internal.Functions
     tb_present,
     tb_print,
     tb_print_ex,
-    tb_printf,
-    tb_printf_ex,
     tb_send,
-    tb_sendf,
     tb_set_cell,
     tb_set_cell_ex,
     tb_set_clear_attrs,
@@ -40,155 +37,114 @@ module Termbox2.Bindings.C.Internal.Functions
   )
 where
 
--- int tb_attr_width(void);
--- int tb_clear(void);
--- int tb_extend_cell(int x, int y, uint32_t ch);
--- int tb_get_fds(int *ttyfd, int *resizefd);
--- int tb_has_egc(void);
--- int tb_has_truecolor(void);
--- int tb_height(void);
--- int tb_hide_cursor(void);
--- int tb_init(void);
--- int tb_init_fd(int ttyfd);
--- int tb_init_file(const char *path);
--- int tb_init_rwfd(int rfd, int wfd);
--- int tb_invalidate(void);
--- int tb_last_errno(void);
--- int tb_peek_event(struct tb_event *event, int timeout_ms);
--- int tb_poll_event(struct tb_event *event);
--- int tb_present(void);
--- int tb_print(int x, int y, uintattr_t fg, uintattr_t bg, const char *str);
--- int tb_print_ex(int x, int y, uintattr_t fg, uintattr_t bg, size_t *out_w, const char *str);
--- int tb_printf(int x, int y, uintattr_t fg, uintattr_t bg, const char *fmt, ...);
--- int tb_printf_ex(int x, int y, uintattr_t fg, uintattr_t bg, size_t *out_w, const char *fmt, ...);
--- int tb_send(const char *buf, size_t nbuf);
--- int tb_sendf(const char *fmt, ...);
--- int tb_set_cell(int x, int y, uint32_t ch, uintattr_t fg, uintattr_t bg);
--- int tb_set_cell_ex(int x, int y, uint32_t *ch, size_t nch, uintattr_t fg, uintattr_t bg);
--- int tb_set_clear_attrs(uintattr_t fg, uintattr_t bg);
--- int tb_set_cursor(int cx, int cy);
--- int tb_set_func(int fn_type, int (*fn)(struct tb_event *, size_t *));
--- int tb_set_input_mode(int mode);
--- int tb_set_output_mode(int mode);
--- int tb_shutdown(void);
--- const char *tb_strerror(int err);
--- int tb_utf8_char_length(char c);
--- int tb_utf8_char_to_unicode(uint32_t *out, const char *c);
--- int tb_utf8_unicode_to_char(char *out, uint32_t c);
--- const char *tb_version(void);
--- int tb_width(void);
--- struct tb_cell *tb_cell_buffer(void);
+import Data.Word (Word32, Word64)
+import Foreign.C (CChar (..), CInt (..), CSize (..), CString)
+import Foreign.C.ConstPtr (ConstPtr (..))
+import Foreign.Ptr (FunPtr, Ptr)
+import Termbox2.Bindings.C.Internal.Cell (Tb_cell)
+import Termbox2.Bindings.C.Internal.Event (Tb_event)
 
-tb_attr_width :: ()
-tb_attr_width = undefined
+foreign import capi unsafe "termbox2.h tb_attr_width"
+  tb_attr_width :: CInt
 
-tb_cell_buffer :: ()
-tb_cell_buffer = undefined
+foreign import capi unsafe "termbox2.h tb_cell_buffer"
+  tb_cell_buffer :: IO (Ptr Tb_cell)
 
-tb_clear :: ()
-tb_clear = undefined
+foreign import capi unsafe "termbox2.h tb_clear"
+  tb_clear :: IO CInt
 
-tb_extend_cell :: ()
-tb_extend_cell = undefined
+foreign import capi unsafe "termbox2.h tb_extend_cell"
+  tb_extend_cell :: CInt -> CInt -> Word32 -> IO CInt
 
-tb_get_fds :: ()
-tb_get_fds = undefined
+foreign import capi unsafe "termbox2.h tb_get_fds"
+  tb_get_fds :: Ptr CInt -> Ptr CInt -> IO CInt
 
-tb_has_egc :: ()
-tb_has_egc = undefined
+foreign import capi unsafe "termbox2.h tb_has_egc"
+  tb_has_egc :: CInt
 
-tb_has_truecolor :: ()
-tb_has_truecolor = undefined
+foreign import capi unsafe "termbox2.h tb_has_truecolor"
+  tb_has_truecolor :: CInt
 
-tb_height :: ()
-tb_height = undefined
+foreign import capi unsafe "termbox2.h tb_height"
+  tb_height :: IO CInt
 
-tb_hide_cursor :: ()
-tb_hide_cursor = undefined
+foreign import capi unsafe "termbox2.h tb_hide_cursor"
+  tb_hide_cursor :: IO CInt
 
-tb_init :: ()
-tb_init = undefined
+foreign import capi unsafe "termbox2.h tb_init"
+  tb_init :: IO CInt
 
-tb_init_fd :: ()
-tb_init_fd = undefined
+foreign import capi unsafe "termbox2.h tb_init_fd"
+  tb_init_fd :: CInt -> IO CInt
 
-tb_init_file :: ()
-tb_init_file = undefined
+foreign import capi unsafe "termbox2.h tb_init_file"
+  tb_init_file :: CString -> IO CInt
 
-tb_init_rwfd :: ()
-tb_init_rwfd = undefined
+foreign import capi unsafe "termbox2.h tb_init_rwfd"
+  tb_init_rwfd :: CInt -> CInt -> IO CInt
 
-tb_invalidate :: ()
-tb_invalidate = undefined
+foreign import capi unsafe "termbox2.h tb_invalidate"
+  tb_invalidate :: IO CInt
 
-tb_last_errno :: ()
-tb_last_errno = undefined
+foreign import capi unsafe "termbox2.h tb_last_errno"
+  tb_last_errno :: IO CInt
 
-tb_peek_event :: ()
-tb_peek_event = undefined
+foreign import capi interruptible "termbox2.h tb_peek_event"
+  tb_peek_event :: Ptr Tb_event -> CInt -> IO CInt
 
-tb_poll_event :: ()
-tb_poll_event = undefined
+foreign import capi interruptible "termbox2.h tb_poll_event"
+  tb_poll_event :: Ptr Tb_event -> IO CInt
 
-tb_present :: ()
-tb_present = undefined
+foreign import capi unsafe "termbox2.h tb_present"
+  tb_present :: IO CInt
 
-tb_print :: ()
-tb_print = undefined
+foreign import capi unsafe "termbox2.h tb_print"
+  tb_print :: CInt -> CInt -> Word64 -> Word64 -> CString -> IO CInt
 
-tb_print_ex :: ()
-tb_print_ex = undefined
+foreign import capi unsafe "termbox2.h tb_print_ex"
+  tb_print_ex :: CInt -> CInt -> Word64 -> Word64 -> Ptr CSize -> CString -> IO CInt
 
-tb_printf :: ()
-tb_printf = undefined
+foreign import capi unsafe "termbox2.h tb_send"
+  tb_send :: CString -> CSize -> IO CInt
 
-tb_printf_ex :: ()
-tb_printf_ex = undefined
+foreign import capi unsafe "termbox2.h tb_set_cell"
+  tb_set_cell :: CInt -> CInt -> Word32 -> Word64 -> Word64 -> IO CInt
 
-tb_send :: ()
-tb_send = undefined
+foreign import capi unsafe "termbox2.h tb_set_cell_ex"
+  tb_set_cell_ex :: CInt -> CInt -> Ptr Word32 -> CSize -> Word64 -> Word64 -> IO CInt
 
-tb_sendf :: ()
-tb_sendf = undefined
+foreign import capi unsafe "termbox2.h tb_set_clear_attrs"
+  tb_set_clear_attrs :: Word64 -> Word64 -> IO CInt
 
-tb_set_cell :: ()
-tb_set_cell = undefined
+foreign import capi unsafe "termbox2.h tb_set_cursor"
+  tb_set_cursor :: CInt -> CInt -> IO CInt
 
-tb_set_cell_ex :: ()
-tb_set_cell_ex = undefined
+foreign import capi unsafe "termbox2.h tb_set_func"
+  tb_set_func :: CInt -> FunPtr (Ptr Tb_event -> Ptr CSize -> IO CInt) -> IO CInt
 
-tb_set_clear_attrs :: ()
-tb_set_clear_attrs = undefined
+foreign import capi unsafe "termbox2.h tb_set_input_mode"
+  tb_set_input_mode :: CInt -> IO CInt
 
-tb_set_cursor :: ()
-tb_set_cursor = undefined
+foreign import capi unsafe "termbox2.h tb_set_output_mode"
+  tb_set_output_mode :: CInt -> IO CInt
 
-tb_set_func :: ()
-tb_set_func = undefined
+foreign import capi unsafe "termbox2.h tb_shutdown"
+  tb_shutdown :: IO CInt
 
-tb_set_input_mode :: ()
-tb_set_input_mode = undefined
+foreign import capi unsafe "termbox2.h tb_strerror"
+  tb_strerror :: CInt -> IO (ConstPtr CChar)
 
-tb_set_output_mode :: ()
-tb_set_output_mode = undefined
+foreign import capi unsafe "termbox2.h tb_utf8_char_length"
+  tb_utf8_char_length :: CChar -> IO CInt
 
-tb_shutdown :: ()
-tb_shutdown = undefined
+foreign import capi unsafe "termbox2.h tb_utf8_char_to_unicode"
+  tb_utf8_char_to_unicode :: Ptr Word32 -> ConstPtr CChar -> IO CInt
 
-tb_strerror :: ()
-tb_strerror = undefined
+foreign import capi unsafe "termbox2.h tb_utf8_unicode_to_char"
+  tb_utf8_unicode_to_char :: Ptr CChar -> Word32 -> IO CInt
 
-tb_utf8_char_length :: ()
-tb_utf8_char_length = undefined
+foreign import capi unsafe "termbox2.h tb_version"
+  tb_version :: ConstPtr CChar
 
-tb_utf8_char_to_unicode :: ()
-tb_utf8_char_to_unicode = undefined
-
-tb_utf8_unicode_to_char :: ()
-tb_utf8_unicode_to_char = undefined
-
-tb_version :: ()
-tb_version = undefined
-
-tb_width :: ()
-tb_width = undefined
+foreign import capi unsafe "termbox2.h tb_width"
+  tb_width :: IO CInt
