d451cc1d234c9cba8ee3892cb97f5837dc74f88a
Mitchell Rosen <mitchellwrosen@gmail.com>
2023-11-06 19:33:41 -0500

    fix Tb_cell attribute sizes

--- termbox2-bindings-c/src/Termbox2/Bindings/C/Internal/Cell.hs
+++ termbox2-bindings-c/src/Termbox2/Bindings/C/Internal/Cell.hs
@@ -3,7 +3,7 @@ module Termbox2.Bindings.C.Internal.Cell
   )
 where
 
-import Data.Word (Word16, Word32)
+import Data.Word (Word32, Word64)
 import Foreign.C (CSize)
 import Foreign.Ptr (Ptr)
 import Foreign.Storable (Storable)
@@ -15,14 +15,14 @@ data {-# CTYPE "termbox2.h" "struct tb_cell" #-} Tb_cell = Tb_cell
   { -- | A unicode character.
     ch :: {-# UNPACK #-} !Word32,
     -- | Foreground attribute.
-    fg :: {-# UNPACK #-} !Word16,
+    fg :: {-# UNPACK #-} !Word64,
     -- | Background attribute.
-    bg :: {-# UNPACK #-} !Word16,
+    bg :: {-# UNPACK #-} !Word64,
     -- | A grapheme cluster.
     ech :: {-# UNPACK #-} !(Ptr Word32),
-    -- | Length of @ech@, in bytes; 0 means use @ch@.
+    -- | Length of @ech@; 0 means use @ch@.
     nech :: {-# UNPACK #-} !CSize,
-    -- | Capacity of @ech@, in bytes.
+    -- | Capacity of @ech@.
     cech :: {-# UNPACK #-} !CSize
   }
   deriving stock (Eq, Generic, Ord, Show)
@@ -30,7 +30,7 @@ data {-# CTYPE "termbox2.h" "struct tb_cell" #-} Tb_cell = Tb_cell
 instance Storable Tb_cell where
   sizeOf :: Tb_cell -> Int
   sizeOf _ =
-    8 + sizeofPtr + sizeofCSize + sizeofCSize
+    20 + sizeofPtr + sizeofCSize + sizeofCSize
 
   alignment :: Tb_cell -> Int
   alignment _ =
@@ -41,23 +41,23 @@ instance Storable Tb_cell where
     Tb_cell
       <$> Storable.peekByteOff ptr 0
       <*> Storable.peekByteOff ptr 4
-      <*> Storable.peekByteOff ptr 6
-      <*> Storable.peekByteOff ptr 8
-      <*> Storable.peekByteOff ptr (8 + sizeofPtr)
-      <*> Storable.peekByteOff ptr (8 + sizeofPtr + sizeofCSize)
+      <*> Storable.peekByteOff ptr 12
+      <*> Storable.peekByteOff ptr 20
+      <*> Storable.peekByteOff ptr (20 + sizeofPtr)
+      <*> Storable.peekByteOff ptr (20 + sizeofPtr + sizeofCSize)
 
   poke :: Ptr Tb_cell -> Tb_cell -> IO ()
   poke ptr Tb_cell {ch, fg, bg, ech, nech, cech} = do
     Storable.pokeByteOff ptr 0 ch
     Storable.pokeByteOff ptr 4 fg
-    Storable.pokeByteOff ptr 6 bg
-    Storable.pokeByteOff ptr 8 ech
-    Storable.pokeByteOff ptr (8 + sizeofPtr) nech
-    Storable.pokeByteOff ptr (8 + sizeofPtr + sizeofCSize) cech
+    Storable.pokeByteOff ptr 12 bg
+    Storable.pokeByteOff ptr 20 ech
+    Storable.pokeByteOff ptr (20 + sizeofPtr) nech
+    Storable.pokeByteOff ptr (20 + sizeofPtr + sizeofCSize) cech
 
 sizeofPtr :: Int
 sizeofPtr =
-  Storable.sizeOf (undefined :: Ptr Word32)
+  Storable.sizeOf (undefined :: Ptr ())
 
 sizeofCSize :: Int
 sizeofCSize =
